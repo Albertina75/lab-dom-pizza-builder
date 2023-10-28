ad877dddec551a32d3121cdc203c3f3d0f19a396
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-28 18:21:16 +0200

    error file. vuelvo a iniciar

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,21 +4,3 @@ module.exports = {
     port: 4444
   }
 };
-// elementos de los botones
-const pepperoniButton = document.querySelector('.btn-peppeoni');
-const mushroomsButton = document.querySelector('.btn-mushrooms');
-const greenPeppersButton = document.querySelector('.btn-green-peppers');
-
-
-// agrego click a los botones
-pepperoniButton.addEventListener('click', () => {
-//llamo a las funciones para mostrar o no el ingrediente
-renderPepperoni();
-});
-mushroomsButton.addEventListener('click', () => {
-  renderMushrooms();
-});
-greenPeppersButton.addEventListener('click', () => {
-renderGreenPepperoni();
-});
-
